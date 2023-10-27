c10bcb383725996b0da4eb42ac5538e18b162b80
SubaniMalla <subanimalla1@gmail.com>
2023-10-27 22:09:47 +0545

    Create an enemy that can be defeated with a simple jump

---
+++ godot/frog.gd
@@ -0,0 +1,45 @@
+extends CharacterBody2D
+
+var player
+var SPEED = 50
+var chase = false
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _ready():
+	get_node("AnimatedSprite2D").play("idle")
+func _physics_process(delta):
+	velocity.y += gravity * delta
+	if chase == true:
+		if get_node("AnimatedSprite2D").animation != "jump":
+			get_node("AnimatedSprite2D").play("jump")
+		player = get_node("../../player/player")
+		var direction = (player.position - self.position).normalized()
+		if direction.x > 0:
+			get_node("AnimatedSprite2D").flip_h = true
+		else:
+			get_node("AnimatedSprite2D").flip_h = false
+		velocity.x = direction.x * SPEED
+	else:
+		if get_node("AnimatedSprite2D").animation != "death":
+			get_node("AnimatedSprite2D").play("idle")
+		velocity.x = 0
+	
+	move_and_slide()
+
+
+func _on_player_detection_body_entered(body):
+	if body.name == "player":
+		chase=true
+		
+func _on_player_detection_body_exited(body):
+	if body.name == "player":
+		chase=false
+
+
+func _on_player_death_body_entered(body):
+	
+	if body.name == "player":
+		chase = false
+		get_node("AnimatedSprite2D").play("death")
+		await get_node("AnimatedSprite2D").animation_finished
+		self.queue_free()
